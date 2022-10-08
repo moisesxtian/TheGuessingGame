a70456e2f8123a7dd72cc80dabe2afcd8dbab4b5
moisesxtian <75287447+moisesxtian@users.noreply.github.com>
2022-10-09 00:09:08 +0800

    Add files via upload

---
+++ TheGuessingGame/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace TheGuessingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cow = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.score = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Cocogoose Pro", 9.749999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(127, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Guess";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Coustard", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 52);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "_________";
+            // 
+            // cow
+            // 
+            this.cow.AutoSize = true;
+            this.cow.Location = new System.Drawing.Point(319, 295);
+            this.cow.Name = "cow";
+            this.cow.Size = new System.Drawing.Size(0, 13);
+            this.cow.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Cocogoose Pro", 9.749999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(41, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Hint";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Cocogoose Pro", 9.749999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(213, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 29);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "New Word";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(34, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hints: 0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(17, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(295, 62);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PaleGreen;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(334, 138);
+            this.panel1.TabIndex = 7;
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score.Location = new System.Drawing.Point(208, 256);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(88, 26);
+            this.score.TabIndex = 8;
+            this.score.Text = "Score: 0";
+            this.score.Click += new System.EventHandler(this.score_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.LightYellow;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(333, 59);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(157, 238);
+            this.listBox1.TabIndex = 9;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(333, -2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(160, 65);
+            this.panel2.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(4, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 26);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Wrong Guesses";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(490, 293);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.cow);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label cow;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TheGuessingGame/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace TheGuessingGame
+{
+    public partial class Form1 : Form
+    {
+
+        string theword;
+        string blankword;
+
+
+        string hintedword;
+        int hints=5;
+        int scores=0;
+        Random rand = new Random();
+        int randomchar;
+        StringBuilder strword = new StringBuilder();
+        StringBuilder strblankword = new StringBuilder();
+        
+        private void generateWord() {
+            Random r1=new Random();
+            string[] wordlist={"Infinite","Bytes","Printer","Computer","Shadow", "Four", "Electric", "Kilobytes", "Process", "Camera", "Paper", "Ink", "Properties", "Universal", "bat", "fan" };
+            int wordpicker = r1.Next(wordlist.Length);
+            StringBuilder selectedword=new StringBuilder(wordlist[wordpicker]);
+            getWord(selectedword.ToString());
+            cow.Text = theword.ToString();
+        }
+        private void getWord(string word) {
+            theword = word;
+        }
+
+        private void getBlank(string blank) {
+            blankword = blank;
+        }
+
+         public void showHint() {
+            randomchar = rand.Next(theword.Length);
+            strword = new StringBuilder(theword);
+            strblankword = new StringBuilder(blankword);
+            repeatHint();
+
+        }
+
+        public void repeatHint() {
+            if (blankword == theword)
+            {
+                label1.Text = "Maximum Hints";
+            }
+            else
+            {
+                randomchar = rand.Next(theword.Length);
+                if (blankword[randomchar].ToString() != "_")
+                {
+                    repeatHint();
+                }
+                strblankword[randomchar] = strword[randomchar];
+                hintedword = strblankword.ToString();
+                blankword = hintedword;
+                label1.Text = blankword;
+            }
+        }
+        private void validateHint()
+        {
+            if (hints == 0)
+            {
+                label3.ForeColor = Color.Red;
+            }
+            else
+            {
+                hints = hints - 1;
+                label3.Text ="Hints: "+ hints.ToString();
+                showHint();
+            }
+        }
+        public void validateGuess()
+        {
+            if (textBox1.Text ==theword) {
+                cow.Text = "Correct";
+                scores = scores + 1;
+                hints = hints + 5;
+                score.Text = "Score: " + scores;
+                label3.Text = "Hints: " + hints.ToString();
+                generateWord();
+                turntoblank();
+                showHint();
+            }
+            else
+            {
+                cow.Text = "Wrong";
+                listBox1.Items.Add(textBox1.Text);
+
+            }
+        }
+        private void turntoblank() {
+            string word = theword;
+            string blank;
+            string temp=null;
+            for(int i=0; i<word.Length; i++)
+            {
+                blank = "_";
+                temp = blank+temp;
+                blank = temp;
+                getBlank(blank);
+                label1.Text = blankword;
+            }
+        }
+        
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            label3.Text = "Hints: "+hints;
+            generateWord();
+            turntoblank();
+            showHint();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            validateGuess();
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+         validateHint();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            generateWord();
+            turntoblank();
+            showHint();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void score_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
